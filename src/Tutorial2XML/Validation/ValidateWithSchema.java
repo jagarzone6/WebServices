5aa39c982a11ac6c328d9aa2bce34a366e606c69
jgarzon <jgarzon@easysol.net>
2016-02-01 17:42:21 -0500

    Esquema del servicio

--- src/Tutorial2XML/Validation/ValidateWithSchema.java
+++ src/Tutorial2XML/Validation/ValidateWithSchema.java
@@ -1,15 +1,12 @@
-package Tutorial2XML.Validacion;
+package Tutorial2XML.Validation;
 
 /**
  * Created by jgarzon on 1/02/16.
  */
-import java.io.File;
-import java.io.IOException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+
 import org.w3c.dom.Document;
-import org.xml.sax.SAXException;
 
 public class ValidateWithSchema {
 
@@ -24,12 +21,12 @@ public class ValidateWithSchema {
                 "http://www.w3.org/2001/XMLSchema");
         dbf.setAttribute(
                 "http://java.sun.com/xml/jaxp/properties/schemaSource",
-                "classifieds.xsd");
+                "src/Tutorial2XML/Validation/classifieds.xsd");
 
         Document doc = null;
         try{
             DocumentBuilder parser = dbf.newDocumentBuilder();
-            doc = parser.parse("classifieds.xml");
+            doc = parser.parse("src/Tutorial2XML/Validation/classifieds.xml");
         } catch (Exception e){
             e.printStackTrace();
         }
